2b8bb1f1af2e82c95ac587d8a6279a85b95f3c11
Theta91 <theta91.pb@gmail.com>
2013-05-17 22:36:39 -0400

    updated to automatically split all images

--- split.sh
+++ split.sh
@@ -1,55 +1,35 @@
 #!/bin/bash -
 
 ################################################################################
-#    Copyright (C) 2011 Someone                                                #
-#                                                                              #
-#    This program is free software: you can redistribute it and/or modify      #
-#    it under the terms of the GNU General Public License as published by      #
-#    the Free Software Foundation, either version 3 of the License, or         #
-#    (at your option) any later version.                                       #
-#                                                                              #
-#    This program is distributed in the hope that it will be useful,           #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
-#    GNU General Public License for more details.                              #
-#                                                                              #
-#    You should have received a copy of the GNU General Public License         #
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
-################################################################################
-
-################################################################################
 case "$1" in
   "" | "-h" | "--help" )
     echo -e "Usage:"
-    echo -e "  split.sh [option] [top-level directory]\n"
+    echo -e "  split.sh [top-level path(s)]\n"
 
-    echo -e "split.sh splits ape, wav or flac files based on a cuesheet, transcodes them to flac,"
+    echo -e "split.sh splits ape, wav and flac files based on a cuesheet, transcodes them to flac,"
     echo -e "tags them based on the cuesheet, and renames the files according to track number"
     echo -e "and title.\n"
 
-    echo -e "Options:"
-    echo -e "  -ape\t\tsplit ape files"
-    echo -e "  -flac\t\tsplit flac files"
-    echo -e "  -h  --help\tprint this help and exit"
-    echo -e "  -wav\t\tsplit wav files"
     exit 0
   ;;
-  "-ape" | "-flac" | "-wav" )
-    ext=.${1#-}
-  ;;
   * )
-    echo -e "Invalid option. For help, use split --help"
+    for path in $@
+    do
+      if [[ ! -d "$path" || -h "$path" ]]
+      then
+        echo -e "$path does not exist or is a symbolic link. For help, use 'split.sh --help'"
+      exit 1
+      fi
+    done
   ;;
 esac
 ################################################################################
-if [[ ! -d "${!#}" || -h "${!#}" ]]
-then
-  echo -e "Directory does not exist or is a symbolic link. For help, use 'split.sh --help'"
-  exit 1
-fi
-################################################################################
 IFS=$'\n'
-files=( $(find "${!#}" -name "*$ext" -type f -print) )
+if [[ $(which mac) == 0 ]]
+then
+  ape='-name "*.ape" -o'
+fi
+files=( $(find "$@" \( "$ape" -name "*.flac" -o -name "*.wav" \) -type f -print) )
 
 for (( c=0; c<${#files[@]}; c++ ))
 do
